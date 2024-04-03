93168d7f6dc0a7d2c9b51be130e654a5c8a5ef2f
SumitSharma878 <159026356+SumitSharma878@users.noreply.github.com>
2024-04-03 11:59:29 +0000

    datatypes

---
+++ .devcontainer/Basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict" //trea all js code as newwer version
+// alert(5+9) // we are using node js not browser
+//number =2 to power 53
+//bigint
+// string=""
+//boolean=true/false
+//null-standalone value
+// undefined
+
+//object
+console.log(typeof null) // null is an object in this situation
