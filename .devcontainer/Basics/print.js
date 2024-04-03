30d7280cda849476e54958e173b6c08bc960a156
SumitSharma878 <159026356+SumitSharma878@users.noreply.github.com>
2024-04-03 12:03:48 +0000

    datatypes done

---
+++ .devcontainer/Basics/print.js
@@ -0,0 +1,6 @@
+accountEmail ="sumit@gmail.com"
+accountpass ="980983098"
+accountCity ="patna"
+let state;
+// dont use var bcz ek jgh change krenge to block scope m issue hota h
+console.table([accountEmail,accountpass,accountCity,state])
