c044eccd96d5bdf68d53ada6447a10e486076d33
SumitSharma878 <159026356+SumitSharma878@users.noreply.github.com>
2024-04-04 15:11:02 +0000

    ConversionOfDatatype

---
+++ .devcontainer/Basics/datatypeConversion.js
@@ -0,0 +1,18 @@
+// let score="sjklj84u49";
+// // let scorr=null;
+// // check null and 0 also
+// // "33"-33  ,"44afdj"-NaN
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // Number hmesa koi v value ko number  m convert kr deta h
+// let valueInNumber =Number(score);
+// console.log(typeof valueInNumber);
+// console.log(typeof Number(scorr))
+
+// true=1,false=0 ,"sumit"-->true, " "->false 
+let isLoggedIn="sumit" ;
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//similar way me string m kisi ko v convert kr skte h
+
