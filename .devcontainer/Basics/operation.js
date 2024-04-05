c4e08164096243f0ff06239519f36bff41b8ff86
SumitSharma878 <159026356+SumitSharma878@users.noreply.github.com>
2024-04-05 12:53:10 +0000

    operationAndcomparisondone

---
+++ .devcontainer/Basics/operation.js
@@ -0,0 +1,28 @@
+// let value =88;
+// let negValue=-value;
+// console.log(negValue)
+/*// agr string pehle h to pura number ko str bna deta h baad m agr str h to normal number hogga
+let num=("1"+1+47);
+console.log(num)
+let num1=(1+4+"788")
+console.log(num1)
+console.log(typeof num1)*/
+
+// let score=100;
+// ++score;
+// console.log(score)
+
+// comparison
+// console.log("2">0);
+// console.log("200"<0);
+// console.log(null>0);
+// console.log(null==0);// 
+// console.log(null>=0);
+
+// console.log(undefined ==0)
+// console.log(undefined >0)
+// console.log(undefined >=0)
+
+
+// strict check ===
+console.log("2"===2)
