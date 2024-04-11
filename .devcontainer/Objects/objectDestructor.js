7de4a834c3baaa7f4717cedba3b2d8087b843874
SumitSharma878 <159026356+SumitSharma878@users.noreply.github.com>
2024-04-11 07:39:02 +0000

    objectDestructorBasic Finish

---
+++ .devcontainer/Objects/objectDestructor.js
@@ -0,0 +1,17 @@
+  const course ={
+     courseName :"js in Hindi",
+     price :"888",
+     courseInstructor :"sumit"
+  }
+  // course.courseInstructor
+  const {courseInstructor} =course
+  const {courseInstructor:instructor} =course  // rename kr diye courseInstructor ko
+  console.log(instructor)
+  console.log(courseInstructor)
+// json
+//   {
+// name :"sumit",
+//   "CourseName" :"js in hindi ",
+//   "price": "free"
+//   }
+
